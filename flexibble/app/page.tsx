bb8f319b0872074664329b99cafe8ec363ff655b
droid-anuj <anujyadavvvv12345@gmail.com>
2024-02-10 17:55:27 +0530

    add grafbase config

---
+++ flexibble/app/page.tsx
@@ -0,0 +1,12 @@
+function home() {
+  return (
+    <section className="flex-start flex-col paddings mb-16">
+
+        <h1>categories</h1>
+        <h1>Posts</h1>
+        <h1>loadmore</h1>
+    </section>
+  )
+}
+
+export default home
---
+++ flexibble/grafbase/grafbase.config.ts
@@ -0,0 +1,28 @@
+import { auth, config, graph} from '@grafbase/sdk'
+
+const g = graph.Standalone()
+
+const User = g.type('User', {
+    name: g.string(),
+    email: g.string(),
+    avatarUrl: g.url(),
+    description: g.string(),
+    githubUrl: g.url().optional(),
+    linkInUrl: g.url().optional(),
+    projects: g.ref('Project').list().optional(), 
+  })
+
+const Project =g.type('Project',{
+    title: g.string(),
+    description: g.string(),
+    image: g.url(),
+    liveSiteUrl: g.url(),
+    githubUrl: g.url(),
+    category: g.string(),
+    createdBy: g.ref('User'),
+})
+
+export default config({
+  graph: g
+  
+})
